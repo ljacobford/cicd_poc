e9d26862276b30a5bd8b0f5e4a0516bb00daefce
ljacobford <jacobfo@gmail.com>
2016-10-31 14:14:00 -0700

    fixed typo in script

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -2,4 +2,4 @@
 sudo /opt/bitnami/ctlscript.sh restart apache
 
 sudo chmod +x /home/bitnami/stack/apache2/cgi-bin/index.sh
-sudo chown bitnami.daemon /home/bitnami/stack/apache2/cgi-bin.index.sh
+sudo chown bitnami.daemon /home/bitnami/stack/apache2/cgi-bin/index.sh
