f5cf276fa87c1988b35d3cc3d0415b4b3a3c7ea1
ljacobford <jacobfo@gmail.com>
2016-10-31 14:53:00 -0700

    made color less obnoxious

--- scripts/index.sh
+++ scripts/index.sh
@@ -2,7 +2,7 @@
 echo "Content-type: text/html"
 echo ""
 echo "<html><head><title>My CICD POC"
-echo "</title></head><body bgcolor='00FF00'>"
+echo "</title></head><body bgcolor='CCCCCC'>"
 
 echo "<h1>This is my CI/CD Example</h1>"
 echo "Instance ID: $( curl http://169.254.169.254/latest/meta-data/instance-id )"
