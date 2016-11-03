4c4df0586bfcb18ac1f28817bc7016804d64c168
ljacobford <jacobfo@gmail.com>
2016-11-03 12:54:24 -0700

    fix

--- scripts/index.sh
+++ scripts/index.sh
@@ -2,11 +2,7 @@
 echo "Content-type: text/html"
 echo ""
 echo "<html><head><title>My CICD POC"
-<<<<<<< HEAD
-echo "</title></head><body bgcolor='000000'>"
-=======
 echo "</title></head><body bgcolor='FFFF00'>"
->>>>>>> feature_3
 
 echo "<h1>This is my CI/CD Example</h1>"
 <h2>echo "Instance ID: $( curl http://169.254.169.254/latest/meta-data/instance-id )"</h2>
