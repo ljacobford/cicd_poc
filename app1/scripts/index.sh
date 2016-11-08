ca34ceff02ba3eccd6726c7be743c15fc8ffcfe5
ljacobford <jacobfo@gmail.com>
2016-11-08 11:06:35 -0800

    broke into two apps to reflect actual setup

--- app1/scripts/index.sh
+++ app1/scripts/index.sh
@@ -4,7 +4,7 @@ echo ""
 echo "<html><head><title>My CICD POC"
 echo "</title></head><body bgcolor='009900'>"
 
-echo "<h1>This is my CI/CD Example</h1>"
+echo "<h1>This is App1</h1>"
 echo "<h2>Instance ID: $( curl http://169.254.169.254/latest/meta-data/instance-id )</h2>"
 
 echo "</body></html>"
---
+++ app2/scripts/index.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Content-type: text/html"
+echo ""
+echo "<html><head><title>My CICD POC"
+echo "</title></head><body bgcolor='009900'>"
+
+echo "<h1>This is App2</h1>"
+echo "<h2>Instance ID: $( curl http://169.254.169.254/latest/meta-data/instance-id )</h2>"
+
+echo "</body></html>"
