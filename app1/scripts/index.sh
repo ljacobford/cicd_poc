01fd2e31ad58364646af123bfd98d7008afd7b90
ljacobford <jacobfo@gmail.com>
2016-11-10 14:40:29 -0800

    cc

--- app1/scripts/index.sh
+++ app1/scripts/index.sh
@@ -2,7 +2,7 @@
 echo "Content-type: text/html"
 echo ""
 echo "<html><head><title>My CICD POC"
-echo "</title></head><body bgcolor='000099'>"
+echo "</title></head><body bgcolor='990000'>"
 
 echo "<h1>This is App1</h1>"
 echo "<h2>Instance ID: $( curl http://169.254.169.254/latest/meta-data/instance-id )</h2>"
